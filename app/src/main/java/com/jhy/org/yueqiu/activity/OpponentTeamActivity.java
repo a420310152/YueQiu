f55de0e5d73424106b5b86aa71c9e9d6ffab2b70
a420310152 <420310152@qq.com>
2016-04-19 12:14:42 +0800

    点击约战

---
+++ app/src/main/java/com/jhy/org/yueqiu/activity/OpponentTeamActivity.java
@@ -0,0 +1,16 @@
+package com.jhy.org.yueqiu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jhy.org.yueqiu.R;
+
+/**
+ * Created by Administrator on 2016/4/18.
+ */
+public class OpponentTeamActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
