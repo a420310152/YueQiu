dfd2671e06de97fa719b02b4cec901a2866330af
7hens <thenstango@live.com>
2016-04-25 09:11:55 +0800

    添加Preferences类

--- app/src/main/java/com/jhy/org/yueqiu/activity/ContactActivity.java
+++ app/src/main/java/com/jhy/org/yueqiu/activity/ContactActivity.java
@@ -2,17 +2,88 @@ package com.jhy.org.yueqiu.activity;
 
 import android.app.Activity;
 import com.jhy.org.yueqiu.R;
+import com.jhy.org.yueqiu.adapter.FriendAdapter;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
 /*
  **********************************************
  * 			所有者 X: (夏旺)
  **********************************************
  */
-public class ContactActivity extends Activity {
+public class ContactActivity extends Activity implements AdapterView.OnItemClickListener, View.OnClickListener {
+
+    private TextView tv_title;
+    private ImageButton ibtn_back;
+    private TextView tv_options;
+
+    private FriendAdapter contactAdapter;
+    private ListView lv_contacts;
+    /**
+     * ids 收消息人的 id
+     */
+    List<String> concatList = Arrays.asList("56145", "56146", "56147", "56148");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+/*        tv_title = (TextView) findViewById(R.id.tv_title);
+        tv_title.setText("创建讨论组");
+
+        ibtn_back = (ImageButton) findViewById(R.id.ibtn_back);
+        ibtn_back.setOnClickListener(this);
+
+        tv_options = (TextView) findViewById(R.id.tv_options);
+        tv_options.setOnClickListener(this);
+
+        lv_contacts = (ListView) findViewById(R.id.lv_contacts);
+        contactAdapter = new ContactAdapter(ContactsActivity.this, concatList);
+        lv_contacts.setAdapter(contactAdapter);
+        lv_contacts.setOnItemClickListener(this);*/
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+/*
+        if (RongIM.getInstance() != null)
+            RongIM.getInstance().startPrivateChat(ContactsActivity.this, concatList.get(position), "title");*/
+    }
+
+    @Override
+    public void onClick(View v) {
+ /*       switch (v.getId()) {
+            case R.id.ibtn_back:
+                finish();
+                break;
+            case R.id.tv_options:
+                if (RongIM.getInstance() != null)
+                *//**
+         *创建讨论组时，mLists为要添加的讨论组成员，创建者一定不能在 mLists 中
+         *//*
+                    RongIM.getInstance().getRongIMClient().createDiscussion("Hello Discussion", concatList, new RongIMClient.CreateDiscussionCallback() {
+                        @Override
+                        public void onSuccess(String s) {
+
+                        }
+
+                        @Override
+                        public void onError(RongIMClient.ErrorCode errorCode) {
+
+                        }
+                    });
+                break;
+            default:
+                break;
+        }*/
     }
 }
--- app/src/main/java/com/jhy/org/yueqiu/test/h/backups/RongUtils.java
+++ app/src/main/java/com/jhy/org/yueqiu/test/h/backups/RongUtils.java
@@ -1,6 +1,10 @@
 package com.jhy.org.yueqiu.test.h.backups;
 
+import android.util.Log;
+
 import com.jhy.org.yueqiu.config.MyApplication;
+import com.jhy.org.yueqiu.domain.Person;
+import com.jhy.org.yueqiu.utils.Preferences;
 
 import io.rong.imkit.RongIM;
 import io.rong.imlib.RongIMClient;
@@ -9,28 +13,37 @@ import io.rong.imlib.RongIMClient;
  * Created by Administrator on 2016/4/21 0021.
  */
 public final class RongUtils {
-    public static void connect (String token) {
+    private static boolean isConnected = false;
+
+    public static void connect () {
         MyApplication app = MyApplication.getInstance();
-        if (app != null) {
-            String curPorcessName = MyApplication.getCurProcessName(MyApplication.getInstance());
-            if (curPorcessName.equals(app.getApplicationInfo().packageName)) {
-                RongIM.connect(token, new RongIMClient.ConnectCallback () {
-                    @Override
-                    public void onSuccess(String s) {
-
-                    }
-
-                    @Override
-                    public void onError(RongIMClient.ErrorCode errorCode) {
-
-                    }
-
-                    @Override
-                    public void onTokenIncorrect() {
-
-                    }
-                });
+        String token = Preferences.get("token");
+        if (app != null && !isConnected && !token.equals("")) {
+            String curPorcessName = MyApplication.getCurProcessName(app);
+            String packageName = app.getApplicationInfo().packageName;
+            if (packageName.equals(curPorcessName)) {
+                RongIM.connect(token, connectCallback);
             }
         }
     }
+
+    private static RongIMClient.ConnectCallback connectCallback = new RongIMClient.ConnectCallback() {
+        @Override
+        public void onTokenIncorrect() {
+            isConnected = false;
+            Log.i("ilog", "connect 失败: token不正确!");
+        }
+
+        @Override
+        public void onSuccess(String s) {
+            isConnected = true;
+            Log.i("ilog", "connect 成功");
+        }
+
+        @Override
+        public void onError(RongIMClient.ErrorCode errorCode) {
+            isConnected = true;
+            Log.i("ilog", "connect 失败: " + errorCode.toString());
+        }
+    };
 }
