da6fae3e8623bbe03104a94eb8aa6ac722cbd12e
13071242969 <1170491914@qq.com>
2016-04-27 17:08:32 +0800

    ccs

--- app/src/main/java/com/jhy/org/yueqiu/view/AllTeamLayout.java
+++ app/src/main/java/com/jhy/org/yueqiu/view/AllTeamLayout.java
@@ -3,18 +3,24 @@ package com.jhy.org.yueqiu.view;
 import android.content.Context;
 import android.os.Environment;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.jhy.org.yueqiu.R;
+import com.jhy.org.yueqiu.domain.Person;
 import com.jhy.org.yueqiu.domain.Team;
 
 import java.io.File;
+import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.datatype.BmobPointer;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.GetListener;
 
 /**
@@ -28,6 +34,7 @@ public class AllTeamLayout extends RelativeLayout{
     private TextView tv_allteam_slogan;//球队宣言
     private Context context ;
     private Team team;
+    private Person person;
     public AllTeamLayout(Context context) {
         super(context);
         init(context);
@@ -49,25 +56,53 @@ public class AllTeamLayout extends RelativeLayout{
         tv_team_buildname = (TextView) findViewById(R.id.tv_team_buildname);
         tv_selector_allteam_time = (TextView) findViewById(R.id.tv_selector_allteam_time);
         tv_allteam_slogan = (TextView) findViewById(R.id.tv_allteam_slogan);
+        person = BmobUser.getCurrentUser(context, Person.class);
     }
+    //查询我加入球队的信息并设置
     public void setAllTeamInfo(Team team){
-        this.team = team;
         BmobQuery<Team> teamQuery = new BmobQuery<Team>();
-        teamQuery.getObject(context, team.getCreator().getObjectId(), new GetListener<Team>() {
+        teamQuery.addWhereEqualTo("members", new BmobPointer(person));
+        teamQuery.findObjects(context, new FindListener<Team>() {
             @Override
-            public void onSuccess(Team team) {
-                String path = Environment.getExternalStorageDirectory()+"logo.jpg";
-                BmobFile file=new BmobFile(new File(path));
-
-                tv_selector_allteam_name.setText(team.getName());
-                tv_team_buildname.setText(team.getCreator()+"");
-                tv_allteam_slogan.setText(team.getMotto());
+            public void onSuccess(List<Team> list) {
+                for (Team addTeam : list) {
+                    Log.i("result","!!!!!!!!!!!!!!~!~~~~~~~~~~~");
+                    String path = Environment.getExternalStorageDirectory() + "logo.jpg";
+                    BmobFile file = new BmobFile(new File(path));
+                    tv_selector_allteam_name.setText(addTeam.getName());
+                    tv_team_buildname.setText(addTeam.getCreator()+"");
+                    tv_selector_allteam_time.setText(addTeam.getCreatedAt());
+                    tv_allteam_slogan.setText(addTeam.getMotto());
+                }
             }
 
             @Override
-            public void onFailure(int i, String s) {
+            public void onError(int i, String s) {
 
             }
         });
+
     }
-}
+    //查询并设置我创建球队的信息
+    public void setBuildTeam(Team team) {
+        BmobQuery<Team> query = new BmobQuery<Team>();
+            query.addWhereEqualTo("creator", new BmobPointer(person));
+            query.findObjects(context, new FindListener<Team>() {
+                @Override
+                public void onSuccess(List<Team> list) {
+                    for (Team team1 : list) {
+                        tv_selector_allteam_name.setText(team1.getName());
+                        tv_team_buildname.setText(person.getUsername());
+                        tv_selector_allteam_time.setText(team1.getCreatedAt());
+                        tv_allteam_slogan.setText(team1.getMotto());
+                    }
+                }
+
+                @Override
+                public void onError(int i, String s) {
+                }
+
+            });
+
+        }
+    }
