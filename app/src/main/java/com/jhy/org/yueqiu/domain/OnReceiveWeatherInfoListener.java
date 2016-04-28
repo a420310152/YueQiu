268b4419348e5388854bbe91651c9e68bce4b7af
7hens <thenstango@live.com>
2016-04-28 17:08:51 +0800

    my changelist

---
+++ app/src/main/java/com/jhy/org/yueqiu/domain/OnReceiveWeatherInfoListener.java
@@ -0,0 +1,8 @@
+package com.jhy.org.yueqiu.domain;
+
+/**
+ * Created by Administrator on 2016/4/28 0028.
+ */
+public interface OnReceiveWeatherInfoListener {
+    void onReceiveWeatherInfo (Weather weather);
+}
---
+++ app/src/main/java/com/jhy/org/yueqiu/utils/HeWeatherUtils.java
@@ -0,0 +1,47 @@
+package com.jhy.org.yueqiu.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Administrator on 2016/4/27 0027.
+ */
+public class HeWeatherUtils {
+    private static final String HTTP_URL = "http://apis.baidu.com/heweather/weather/free";
+    private static final String ARG_CITY = "city";
+    private static final String API_KEY = "d28afcb7b903a5b6300b0a8f25ab3793";
+    private static Logx logx = new Logx(HeWeatherUtils.class);
+
+    public static String request (String city) {
+        String result = null;
+        String httpUrl = HTTP_URL + "?" + ARG_CITY + "=" + city;
+        try {
+            URL url = new URL(httpUrl);
+            logx.e("url: " + httpUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("apikey", API_KEY);
+            connection.connect();
+
+            InputStream in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            StringBuffer buffer = new StringBuffer();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+                buffer.append("\r\n");
+            }
+            reader.close();
+            result = buffer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
